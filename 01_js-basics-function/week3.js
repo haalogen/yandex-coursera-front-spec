0a1fbaae3e845f0bb9c7cc815a132778fc1731f7
Stanislav Nikitin <nikitin.develop@gmail.com>
2019-01-26 18:01:42 +0300

    Add "Week 3" files

---
+++ 01_js-basics-function/week3.js
@@ -0,0 +1,36 @@
+/**
+ *  Методы объекта
+ */
+console.warn('Методы объекта')
+var tweet = {
+  likes: 17,
+  getLikes: function () {
+    return this.likes;
+  },
+  setLikes: function (value) {
+    this.likes = parseInt(value) || 0;
+    return this;
+  },
+}
+
+console.log({tweet});
+
+likes = tweet.getLikes()
+console.log({likes})
+
+likes = tweet.setLikes(666).getLikes()
+console.log({likes})
+
+
+/**
+ * Декларация и конфигурирование свойств объекта
+ */
+console.warn('Декларация и конфигурирование свойств объекта')
+tweet2 = {
+  toString: function () {
+    return 'Хэй!'
+  },
+}
+
+// Ключи перечисляемых свойств
+console.log(Object.keys(tweet2)); // ['toString']
